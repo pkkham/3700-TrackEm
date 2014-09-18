f09e425a9884189569658312a728139cf7606694
Ferrago <tkh0006@auburn.edu>
2014-09-18 16:49:48 -0500

    UI & Stopwatch Functionality
    Added new png files for the stopwatch buttons/background.
    Modified xml files.
    
    * App crashes upon launch.

--- TrackEm_Glass/src/com/example/trackem_glass/TrackEmActivity.java
+++ TrackEm_Glass/src/com/example/trackem_glass/TrackEmActivity.java
@@ -10,23 +10,22 @@
  *
 */
 package com.example.trackem_glass;
-
-import android.content.Intent;
+ 
+import android.graphics.Typeface;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.trackem_glass.R;
 public class TrackEmActivity extends FragmentActivity {
 
 	
 	
 // ============================================================================
 //Life Cycle Methods
+	
+    private TextView tempTextView; //Temporary TextView  
+    private Button tempBtn; //Temporary Button 
 	
     @Override
     //The onCreate(...) method is a life cycle method of the Activity.
@@ -37,7 +36,7 @@ public class TrackEmActivity extends FragmentActivity {
     //
     //When user hits the home button, the app is in a 'stopped' state. This
     //state is held in memory and if the user navigates back to the app, the
-    //information of that state can be retrieved again from 'savedInstanceState'.
+    //information of that state can be retrieved again from 'savedInstanceState'. 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState); //call FragmentActivity's onCreate
         
@@ -46,70 +45,23 @@ public class TrackEmActivity extends FragmentActivity {
         //that pertain to external XML or other resources
         setContentView(R.layout.activity_track_em);
         
-        //If we are starting out fresh from startup, we want
-        //to create a fragment and add it to the view.
-        //R.id.container is the UI spot for this fragment.
-        if (savedInstanceState == null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
-                    .commit();
-        }
+
+        /*-------Setting the TextView Fonts-----------*/  
+        Typeface font = Typeface.createFromAsset(getAssets(), "coolvetica.ttf");  
+        tempTextView = (TextView) findViewById(R.id.timer);  
+        tempTextView.setTypeface(font);  
+        tempTextView = (TextView) findViewById(R.id.backgroundText);  
+        tempTextView.setTypeface(font);  
+        Button tempBtn = (Button)findViewById(R.id.startButton);  
+        tempBtn.setTypeface(font);  
+        tempBtn = (Button)findViewById(R.id.stopButton);  
+        tempBtn.setTypeface(font);  
+        
     }
 // ============================================================================
 
 
-// ============================================================================
-// Options Menu Call Backs
-  	@Override
-  	public boolean onCreateOptionsMenu(Menu menu) {
-  		//When the options menu is created, we want to 'inflate'
-  		//the XML file that we have set up in res/menu. This creates
-  		//the UI for our options menu...
-  		getMenuInflater().inflate(R.menu.track_em, menu);
-  		return super.onCreateOptionsMenu(menu);
-  	}
-  	
-  	@Override
-  	public boolean onOptionsItemSelected(MenuItem item)
-  	{
-  		//Switch around the id's that are declared in the
-  		//XML file res/menu/track_em.xml (the layout we inflated)
-  		switch (item.getItemId())
-  		{
-  		
-  		//If it is the 'preferences' menu item, start a new Activity that
-  		//will lay on top of this one. It is started with what is called
-  		//and 'Intent' and sent to the underlying Android runtime system
-  		//to be processed and created.
-  		case R.id.ab_preferences:
-  			Intent intent = new Intent(this, SettingsActivity.class);
-  			startActivity(intent);
-  			break;
-  		default:
-  			return false;
-  		}
-  		return true;
-  	}
-// ============================================================================
 
   	
-// ============================================================================
-// This will be deleted eventually...just a 'Hello World' for now...
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_track_em, container, false);
-            return rootView;
-        }
-    }
- //============================================================================
 
 } //TrackEmActivity
